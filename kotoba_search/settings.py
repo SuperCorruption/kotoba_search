c87b786777b0e548fc48077d95db26de7dfa2453
Taketo <lhuyue589@gmail.com>
2025-05-21 16:40:32 +0900

    fix: settings.pyの修正

--- kotoba_search/settings.py
+++ kotoba_search/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_URLCONF = 'kotoba_search.urls'
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kotoba-search.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kotoba-search-test.onrender.com']
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 if not SECRET_KEY:
